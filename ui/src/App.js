a2e444efb540965ba238ea311f505b214afa29fe
Gorka <gorka@aquigorka.com>
2023-05-20 14:52:26 -0300

    Add total coins

--- ui/src/App.js
+++ ui/src/App.js
@@ -36,7 +36,7 @@ function useStreak({ uid }) {
   }
 }
 
-function useClaim({ uid, updateStreak }) {
+function useClaim({ uid, updateStreak, updateTotalCoins }) {
   const [claimed, setClaimed] = useState(false)
   const [isClaiming, setIsClaiming] = useState(false)
   const [coins, setCoins] = useState(0)
@@ -58,8 +58,11 @@ function useClaim({ uid, updateStreak }) {
       setCoins(coins)
       setBonus(bonus)
       setNewStreak(streak === 1)
-      updateStreak(streak)
       setClaimed(true)
+
+      const newCoins = coins + bonus.reduce((p, c) => p + c.coins, 0)
+      updateStreak(streak)
+      updateTotalCoins((total) => total + newCoins)
     } catch (err) {
       console.log("Error claiming:", err)
       setClaimError(true)
@@ -78,6 +81,33 @@ function useClaim({ uid, updateStreak }) {
   }
 }
 
+function useTotalCoins({ uid }) {
+  const [totalCoins, setTotalCoins] = useState(0)
+
+  useEffect(() => {
+    if (!uid) {
+      return
+    }
+
+    const run = async () => {
+      try {
+        const res = await fetch(`http://localhost:3000/auth/coins/${uid}`)
+        const { coins } = await res.json()
+        console.log({ coins })
+        setTotalCoins(coins)
+      } catch (err) {
+        console.log("Error fetching coins:", err)
+      }
+    }
+    run()
+  }, [uid])
+
+  return {
+    totalCoins,
+    updateTotalCoins: setTotalCoins,
+  }
+}
+
 function useAuth() {
   const [isSignedIn, setIsSignedIn] = useState(true)
   const [uid, setUid] = useState("user-id")
@@ -88,16 +118,22 @@ function useAuth() {
 
 function useApp() {
   const Auth = useAuth()
+  const Coins = useTotalCoins({ uid: Auth.uid })
   const Streak = useStreak({ uid: Auth.uid })
-  const Claim = useClaim({ uid: Auth.uid, updateStreak: Streak.updateStreak })
+  const Claim = useClaim({
+    uid: Auth.uid,
+    updateStreak: Streak.updateStreak,
+    updateTotalCoins: Coins.updateTotalCoins,
+  })
 
-  return { ...Auth, ...Streak, ...Claim }
+  return { ...Auth, ...Coins, ...Streak, ...Claim }
 }
 
-function Layout({ children, isSignedIn, displayName }) {
+function Layout({ children, isSignedIn, displayName, totalCoins }) {
   return (
     <div>
       <div>{displayName}</div>
+      <div>Coins: {totalCoins}</div>
       <div>{children}</div>
     </div>
   )
@@ -121,18 +157,28 @@ function App() {
     handleClaim,
     newStreak,
     claimed,
+    // coins
+    totalCoins,
   } = useApp()
 
   if (isLoading) {
     return (
-      <Layout isSignedIn={isSignedIn} displayName={displayName}>
+      <Layout
+        isSignedIn={isSignedIn}
+        displayName={displayName}
+        totalCoins={totalCoins}
+      >
         <div>loading...</div>
       </Layout>
     )
   }
 
   return (
-    <Layout isSignedIn={isSignedIn} displayName={displayName}>
+    <Layout
+      isSignedIn={isSignedIn}
+      displayName={displayName}
+      totalCoins={totalCoins}
+    >
       {streak !== 0 && <div>current streak: {streak}</div>}
       {streak === 0 && canClaimToday && <div>start claiming today</div>}
       {streakError && <div>error: try again in a few minutes</div>}
